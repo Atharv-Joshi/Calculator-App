c16f3e61a8a9a388e3bdb6608648c4d7be9e9bec
Atharv-Joshi-TE <atharv@teachedison.com>
2022-11-23 22:27:23 +0530

    calculator app

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,10 +2,126 @@ package com.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private var tvInput : TextView? = null
+    private var isLastNumeric : Boolean = false
+    private  var isLastDecimal : Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tvInput = findViewById(R.id.tvInput)
+    }
+
+    fun onDigit(view : View){
+        tvInput?.append((view as Button).text)
+        isLastDecimal = false
+        isLastNumeric = true
+    }
+
+    fun onClear(view : View){
+        tvInput?.text = ""
+    }
+
+    fun onDecimalPoint(view: View){
+        if(isLastNumeric && !isLastDecimal){
+            tvInput?.append(".")
+            isLastDecimal = true
+            isLastNumeric = false
+        }
+    }
+
+    fun onOperator(view: View){
+        tvInput?.text?.let {
+            if(isLastNumeric && !isOperatorAdded(it.toString())){
+                tvInput?.append((view as Button).text)
+                isLastNumeric = false
+                isLastDecimal = false
+            }
+        }
+    }
+
+    private fun isOperatorAdded(value : String) : Boolean {
+        return if(value.startsWith("-")){
+            false
+        }else{
+            value.contains("/") || value.contains("*") || value.contains("+") || value.contains("-")
+        }
+    }
+
+    private fun removeZeroFromResult(result : String) : String{
+        var value = result
+        if(result.contains(".0")){
+            value = result.substring(0, result.length - 2)
+        }
+        return value
+    }
+
+    fun onEqual(view: View){
+        if(isLastNumeric){
+            var tvValue : String = tvInput?.text.toString()
+            var prefix = ""
+            try {
+
+                if(tvValue.startsWith("-")){
+                    prefix = "-"
+                    tvValue = tvValue.substring(1)
+                }
+
+                if(tvValue.contains("-")){
+                    val splitValue = tvValue.split("-")
+
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+
+                    if(prefix.isNotEmpty()){
+                        one = prefix + one
+                    }
+
+                    tvInput?.text = removeZeroFromResult((one.toDouble() - two.toDouble()).toString())
+                }else if (tvValue.contains("+")){
+                    val splitValue = tvValue.split("+")
+
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+
+                    if(prefix.isNotEmpty()){
+                        one = prefix + one
+                    }
+
+                    tvInput?.text = removeZeroFromResult((one.toDouble() + two.toDouble()).toString())
+                }else if (tvValue.contains("*")){
+                    val splitValue = tvValue.split("*")
+
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+
+                    if(prefix.isNotEmpty()){
+                        one = prefix + one
+                    }
+
+                    tvInput?.text = removeZeroFromResult((one.toDouble() * two.toDouble()).toString())
+                }else if(tvValue.contains("/")){
+                    val splitValue = tvValue.split("/")
+
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+
+                    if(prefix.isNotEmpty()){
+                        one = prefix + one
+                    }
+
+                    tvInput?.text = removeZeroFromResult((one.toDouble() / two.toDouble()).toString())
+                }
+            }catch (e: ArithmeticException){
+                e.printStackTrace()
+            }
+        }
     }
 }
